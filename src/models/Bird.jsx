9a140c4af25c2d6542ec02872017b632498c969c
victoriamazilu <138828561+victoriamazilu@users.noreply.github.com>
2023-12-30 16:39:15 -0500

    New island and birds
    Updated island, updated motion of birds

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -13,33 +13,54 @@ const Bird = () => {
     actions['Scene'].play();
   }, [])
 
-  useFrame(({clock, camera}) => {
-    //In order for the motion to look like a bird, opposed to a straight line, alter its motion like the sine function
-    birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.5 + 2;
+  // useFrame(({clock, camera}) => {
+  //   //In order for the motion to look like a bird, opposed to a straight line, alter its motion like the sine function
+  //   birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.5 + 2;
     
-    //If bird has exited the camera
-    if(birdRef.current.position.x > camera.position.x + 10){ 
-      //Change direction by PI
-      birdRef.current.rotation.y = Math.PI;
-    } else if(birdRef.current.position.x < camera.position.x){
-      //Change direction to forward (reset)
-      birdRef.current.rotation.y = 0;
-    }
+  //   //If bird has exited the camera
+  //   if(birdRef.current.position.x > camera.position.x + 10){ 
+  //     //Change direction by PI
+  //     birdRef.current.rotation.y = Math.PI;
+  //   } else if(birdRef.current.position.x < camera.position.x){
+  //     //Change direction to forward (reset)
+  //     birdRef.current.rotation.y = 0;
+  //   }
   
-    //Updating x and z positions
-    if(birdRef.current.rotation.y === 0){
-      //Moving forward
-      birdRef.current.position.x += 0.01;
-      birdRef.current.position.z -= 0.01;
-    } else{
-      //Moving backward
-      birdRef.current.position.x -= 0.01;
-      birdRef.current.position.z += 0.01;
-    }
-  })
+  //   //Updating x and z positions
+  //   if(birdRef.current.rotation.y === 0){
+  //     //Moving forward
+  //     birdRef.current.position.x += 0.01;
+  //     birdRef.current.position.z -= 0.005;
+  //   } else{
+  //     //Moving backward
+  //     birdRef.current.position.x -= 0.01;
+  //     birdRef.current.position.z += 0.005;
+  //   }
+  // })
+
+  useFrame(({ clock, camera }) => {
+    const radius = 9; // Radius for flight path
+    const speed = 0.25; // Movement speed
+    const angle = clock.getElapsedTime() * speed;
+    const depthOffset = -15; // Center the circle deeper in the screen
+    const normalizedZPos = (birdRef.current.position.z - camera.position.z) / depthOffset;
+
+    // Update x and z for circular motion
+    birdRef.current.position.x = camera.position.x + Math.cos(angle) * radius;
+    birdRef.current.position.z = camera.position.z + Math.sin(angle) * radius + depthOffset;
+
+    // Adjust height based on z position (higher when further from screen)
+    const verticalVariation = 5; // Adjust this value for more or less vertical movement
+    birdRef.current.position.y = 1 + (Math.cos(clock.getElapsedTime()) * 0.5 ) + (normalizedZPos * verticalVariation);
+
+    // Rotate the bird to face direction of motion
+    birdRef.current.rotation.y = -angle + Math.PI;
+});
+
+
 
   return (
-    <mesh ref={birdRef} position={[-5, 2, 1]} scale={[0.75, 0.75, 0.75]}>
+    <mesh ref={birdRef} position={[0, 2, 0]} scale={[0.75, 0.75, 0.75]}>
       <primitive object={scene} />
     </mesh>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,13 +13,13 @@ const Home = () => {
 
   const adjustIsland = () => {
     let screenScale= null;
-    let screenPos = [0, -6.5, -43];
-    let rotation = [0.1, 4.7, 0];
+    let screenPos = [0, -30, -40];
+    let rotation = [0, 0, 0];
 
     if(window.innerWidth < 768){
-      screenScale = [0.8, 0.8, 0.8];
+      screenScale = [0.03, 0.03, 0.03];
     } else{
-      screenScale = [0.9, 0.9, 0.9];
+      screenScale = [0.05, 0.05, 0.05];
     }
 
     return [screenScale, screenPos, rotation];
@@ -52,8 +52,8 @@ const Home = () => {
       <Canvas 
         className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing': 'cursor-grab'}`} camera={{near: 0.1, far: 1000}}>
         <Suspense fallback={<Loader />}>
-          <directionalLight postion={[1, 1, 1]} intensity={1} />
-          <ambientLight intensity={0.5}/>
+          <directionalLight postion={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={1}/>
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
           <pointLight/>
 
