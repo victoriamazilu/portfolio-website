a71a1553b3afdf0638e393f0f8a50ce85635360f
victoriamazilu <138828561+victoriamazilu@users.noreply.github.com>
2024-01-01 22:57:02 -0500

    Update plane motion
    Testing out new plane motion so that y-position is not constantly static

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,4 +1,5 @@
 import {useRef, useEffect} from 'react'
+import {useFrame} from '@react-three/fiber'
 import planeScene from '../assets/3d/plane.glb'
 import {useAnimations, useGLTF} from '@react-three/drei'
 
@@ -6,6 +7,10 @@ const Plane = ({isRotating, ...props}) => {
   const ref = useRef();
   const {scene, animations} = useGLTF(planeScene);
   const {actions} = useAnimations(animations, ref);
+  let isAscending = true;
+  let originalY;
+  let currentY;
+let speed = 0.05;
   
   useEffect(() => {
     if(isRotating) {
@@ -13,8 +18,35 @@ const Plane = ({isRotating, ...props}) => {
     } else {
       actions['Take 001'].stop();
     }
-  }, [actions, isRotating]);
+    originalY = ref.current.position.y;
+    currentY = originalY;
+    }, [actions, isRotating]);
 
+  const origYPO = originalY + 1;
+  const origYMO = originalY - 1;
+  useFrame(() => {
+    if (isRotating) {
+      if (isAscending) {
+        // Ascend until reaching the threshold
+        if (currentY < originalY + 1) {
+          currentY += speed;
+        } else {
+          isAscending = false; // Change direction
+        }
+      } else {
+        // Descend until reaching the lower threshold
+        if (currentY > originalY - 1) {
+          currentY -= speed;
+        } else {
+          isAscending = true; // Change direction
+        }
+      }
+      ref.current.position.y = currentY; // Apply the updated position
+    } else {
+      // Update originalY with the last position when rotation stops
+      originalY = currentY;
+    }
+  });
   return (
     <mesh {...props} ref={ref}>
       <primitive object={scene} />
