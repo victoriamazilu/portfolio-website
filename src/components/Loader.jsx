057f867c6e9de48232a44ec2b041be552b84eb32
victoriamazilu <138828561+victoriamazilu@users.noreply.github.com>
2024-01-03 21:21:05 -0500

    Update loading screen

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,46 @@
-import {Html} from '@react-three/drei'
+import { Html } from '@react-three/drei';
 
 const Loader = () => {
   return (
     <Html>
-        <div className="flex justify-center items-center">
-            <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin"/>
+      <div className="flex justify-center items-center w-full h-full absolute">
+        <div>
+          <div className="loader">
+            <div className="inner-loader"></div>
+          </div>
+          <h1 className="text-poppins text-semibold text-center mt-4 text-slate-600">Loading...</h1>
         </div>
-    </Html>
-  )
-}
+      </div>
+      <style>{`
+        .loader {
+          width: 80px;
+          height: 80px;
+          border: 4px solid rgba(0, 0, 0, 0.1);
+          border-left-color: #3486eb;
+          border-radius: 50%;
+          animation: spin 1s linear infinite;
+        }
 
-export default Loader
+        .inner-loader {
+          width: 100%;
+          height: 100%;
+          border: 4px solid rgba(0, 0, 0, 0.1);
+          border-left-color: #34bdeb;
+          border-radius: 50%;
+          animation: spin 1.5s linear reverse infinite;
+        }
+
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </Html>
+  );
+};
+
+export default Loader;
