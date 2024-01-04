ee1656b2d60e7f2c20c11bd4bf4b896625ef5027
victoriamazilu <138828561+victoriamazilu@users.noreply.github.com>
2024-01-03 19:14:42 -0500

    Declutter
    Delete unnecessary files

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,13 +1,9 @@
-import hero from './hero.jpeg'
-import logo from './logo.svg'
 import fyi from './fyi.png'
 import hci from './hci.png'
 import risetothrive from './risetothrive.png'
 import blueblood from './blueblood.png'
 
 export {
-    hero,
-    logo,
     fyi,
     hci,
     risetothrive,
