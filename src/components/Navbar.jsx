6fca20bc506f9e8e42c3390d4021c7644a2de07e
victoriamazilu <138828561+victoriamazilu@users.noreply.github.com>
2024-01-03 20:53:18 -0500

    Compressed island and edit navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,10 @@ const navbar = () => {
             <p className="blue-gradient_text">V.M.</p>
         </NavLink>
         <nav className="flex text-lg gap-7 font-medium">
-            <NavLink to="/about" className={({isActive}) => isActive ? 'text-blue-800' : 'text-blue-500'}>
+            <NavLink to="/about" className={({isActive}) => isActive ? 'text-blue-800 w-20 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md' : 'text-blue-500 w-20 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'}>
                 About
             </NavLink>
-            <NavLink to="/projects" className={({isActive}) => isActive ? 'text-blue-800' : 'text-blue-500'}>
+            <NavLink to="/projects" className={({isActive}) => isActive ? 'text-blue-800 w-28 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md' : 'text-blue-500 w-28 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'}>
                 Projects
             </NavLink>
         </nav>
