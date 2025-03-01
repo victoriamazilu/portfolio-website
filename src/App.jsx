6fdfa70f1e0bf9971575b87d98afe2c00fa1e877
victoriamazilu <vmazilu@uwaterloo.ca>
2025-02-28 22:56:29 -0500

    Rename About to Experience and update navigation paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,30 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import { Home, About, Projects, Contact } from "./pages";
+import { Home, Experience, Projects, Contact } from "./pages";
 
 const App = () => {
-    return (
-        <main className="bg-slate-300/20 h-FULL">
-            <Router>
-                <Navbar />
+  return (
+    <main className="bg-slate-300/20 h-FULL">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/*"
+            element={
+              <>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route
-                        path="/*"
-                        element={
-                            <>
-                                <Routes>
-                                    <Route path="/about" element={<About />} />
-                                    <Route
-                                        path="/projects"
-                                        element={<Projects />}
-                                    />
-                                    <Route
-                                        path="/contact"
-                                        element={<Contact />}
-                                    />
-                                </Routes>
-                            </>
-                        }
-                    />
+                  <Route path="/experience" element={<Experience />} />
+                  <Route path="/projects" element={<Projects />} />
+                  <Route path="/contact" element={<Contact />} />
                 </Routes>
-            </Router>
-        </main>
-    );
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </main>
+  );
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const navbar = () => {
   return (
     <header className="header">
-        <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"> 
-            <p className="blue-gradient_text">V.M.</p>
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">V.M.</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/experience"
+          className={({ isActive }) =>
+            isActive
+              ? "text-blue-800 w-32 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+              : "text-blue-500 w-32 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+          }
+        >
+          Experience
         </NavLink>
-        <nav className="flex text-lg gap-7 font-medium">
-            <NavLink to="/about" className={({isActive}) => isActive ? 'text-blue-800 w-20 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md' : 'text-blue-500 w-20 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'}>
-                About
-            </NavLink>
-            <NavLink to="/projects" className={({isActive}) => isActive ? 'text-blue-800 w-28 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md' : 'text-blue-500 w-28 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'}>
-                Projects
-            </NavLink>
-        </nav>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive
+              ? "text-blue-800 w-24 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+              : "text-blue-500 w-24 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+          }
+        >
+          Projects
+        </NavLink>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default navbar
+export default navbar;
